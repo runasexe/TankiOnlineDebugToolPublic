c9519be612ec37a0b087656da32a6d69423b7da0
runasexe <runas.exe@gmail.com>
2023-01-22 08:59:04 +0300

    Create TankiOnlineDebug.user.js

---
+++ build/userscript/TankiOnlineDebug.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name           TankiOnlineDebug.StaticProduction.All
+// @namespace      TankiOnlineDebug
+// @version        1.1.0
+// @description    Debug tool for TankiOnline. Exists features: fast open container, extended battle notifications
+// @description:ru Утилита для отладки игры "Танки Онлайн". Возможности: быстрое открытие контейнеров, расширенные уведомления игры
+// @author         runasexe
+// @homepage       https://github.com/runasexe/TankiOnlineDebugToolPublic
+// @homepageURL    https://github.com/runasexe/TankiOnlineDebugToolPublic
+// @website        https://github.com/runasexe/TankiOnlineDebugToolPublic
+// @source         https://github.com/runasexe/TankiOnlineDebugToolPublic
+// @match          https://tankionline.com/hook/*
+// @match          https://*.tankionline.com/hook/*
+// @icon           https://tankionline.com/favicon.ico
+// @require        https://raw.githubusercontent.com/runasexe/TankiOnlineDebugToolPublic/main/build/static-production.js
+// @run-at         document-start
+// @updateURL      https://raw.githubusercontent.com/runasexe/TankiOnlineDebugToolPublic/main/build/userscript/TankiOnlineDebug.user.js
+// @downloadURL    https://raw.githubusercontent.com/runasexe/TankiOnlineDebugToolPublic/main/build/userscript/TankiOnlineDebug.user.js
+// @supportURL     https://github.com/runasexe/TankiOnlineDebugToolPublic/issues
+// @grant          none
+// ==/UserScript==
+
+/* Using external script */
+
